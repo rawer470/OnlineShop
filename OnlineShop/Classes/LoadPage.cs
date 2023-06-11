f0dc35e9cae2aad8118d9bf87ee6abb68f8bede4
rawer470 <75203120+rawer470@users.noreply.github.com>
2023-06-12 00:42:22 +0300

    Make search

--- OnlineShop/Classes/LoadPage.cs
+++ OnlineShop/Classes/LoadPage.cs
@@ -16,7 +16,7 @@ namespace OnlineShop.Classes
         public static Button ObrazecProduct { get; set; }
         public static Button Korzina { get; set; }
 
-        private static async Task<List<Uri>> LoadImage(List<Product> products)
+        public static async Task<List<Uri>> LoadImage(List<Product> products)
         {
             List<Uri> ret = new List<Uri>();
             foreach (Product product in products)
@@ -26,7 +26,7 @@ namespace OnlineShop.Classes
             }
             return ret;
         }
-
+       
         public static Button LoadProductButton(Product product)
         {
 
@@ -99,66 +99,7 @@ namespace OnlineShop.Classes
 
         public static async Task<List<WrapPanel>> LoadCategoryPage(string category)
         {
-            List<Product> products = await ProductProcessor.GetFromCategory(category);
-            List<Uri> Images = await LoadImage(products);
-            List<WrapPanel> itog = new List<WrapPanel>();
-            Button[] buttonsProd = new Button[Images.Count];
-            /*  for (int i = 0; i < Images.Count; i++)
-              {
-                  buttonsProd[i] = ObrazecProduct;
-              }*/
-            for (int i = 0; i < buttonsProd.Length; i++)
-            {
-
-                WrapPanel wr = new WrapPanel();
-                wr.Orientation = Orientation.Vertical;
-                //Добавление изображения
-                Image image = new Image();
-                image.Source = new BitmapImage(Images[i]);
-                image.Width = 280;
-                image.Height = 200;
-                image.Margin = new Thickness(10);
-                wr.Children.Add(image);
-
-                //Добавление Цены
-                Label labelPrice = new Label();
-                labelPrice.Content = $"{products[i].Price}$";
-                labelPrice.FontSize = 25;
-                wr.Children.Add(labelPrice);
-
-                //Добавление рейтинга
-                Label labelRat = new Label();
-                labelRat.Content = $"Рейтинг: {products[i].Rating.Rate}, отзывов: {products[i].Rating.Count}";
-                labelRat.FontSize = 18;
-                wr.Children.Add(labelRat);
-
-                //Добавление заголовка
-                Label labelTitle = new Label();
-                labelTitle.Content = products[i].Title;
-                labelTitle.FontSize = 20;
-                wr.Children.Add(labelTitle);
-
-                //Добавление описания
-                Label labelDiscr = new Label();
-                labelDiscr.Content = products[i].Description;
-                labelDiscr.FontSize = 16;
-                wr.Children.Add(labelDiscr);
-
-
-                Button kor = new Button();
-                kor.Content = Korzina.Content;
-                kor.FontSize = Korzina.FontSize;
-                kor.Width = Korzina.Width;
-                kor.Height = Korzina.Height;
-                wr.Children.Add(kor);
-
-                wr.Width = 400;
-                wr.Height = 500;
-                wr.Margin = new Thickness(10);
-                itog.Add(wr);
-                wr.Children.Clear();
-            }
-            return itog;
+            return null;
         }
     }
 }
--- OnlineShop/MainWindow.xaml.cs
+++ OnlineShop/MainWindow.xaml.cs
@@ -37,6 +37,8 @@ namespace OnlineShop
             LoadProduct();
             LoadPage.ObrazecProduct = Elecronic1;
             LoadPage.Korzina = ButtonKorzina;
+
+
         }
 
 
@@ -47,6 +49,48 @@ namespace OnlineShop
             jewelery = await ProductProcessor.GetFromCategory("jewelery");
             mensClothing = await ProductProcessor.GetFromCategory("men's clothing");
             WomansClothing = await ProductProcessor.GetFromCategory("women's clothing");
+
+
+            List<Uri> imel = await LoadPage.LoadImage(elecronics);
+            foreach (Uri uri in imel)
+            {
+                Image image = new Image();
+                image.Width = 150;
+                image.Height = 150;
+                image.Source = new BitmapImage(uri);
+                image.Margin = new Thickness(10);
+                ElMain.Children.Add(image);
+            }
+            List<Uri> imj = await LoadPage.LoadImage(jewelery);
+            foreach (Uri uri in imj)
+            {
+                Image image = new Image();
+                image.Width = 150;
+                image.Height = 150;
+                image.Source = new BitmapImage(uri);
+                image.Margin = new Thickness(10);
+                JMain.Children.Add(image);
+            }
+            List<Uri> imen = await LoadPage.LoadImage(mensClothing);
+            foreach (Uri uri in imen)
+            {
+                Image image = new Image();
+                image.Width = 150;
+                image.Height = 150;
+                image.Source = new BitmapImage(uri);
+                image.Margin = new Thickness(10);
+                MenMain.Children.Add(image);
+            }
+            List<Uri> imewo = await LoadPage.LoadImage(WomansClothing);
+            foreach (Uri uri in imewo)
+            {
+                Image image = new Image();
+                image.Width = 150;
+                image.Height = 150;
+                image.Source = new BitmapImage(uri);
+                image.Margin = new Thickness(10);
+                WomenMain.Children.Add(image);
+            }
         }
 
 
@@ -63,6 +107,8 @@ namespace OnlineShop
 
         private void GoMainPage(object sender, RoutedEventArgs e)
         {
+            MainLogic.Visibility = Visibility.Visible;
+            MainSearch.Visibility = Visibility.Collapsed;
             ElectronicsPage.IsSelected = false;
             MainPage.IsSelected = true;
             CatalogPage.IsSelected = false;
@@ -115,6 +161,7 @@ namespace OnlineShop
         }
         private void GoWomenPage(object sender, RoutedEventArgs e)
         {
+          
             ElectronicsPage.IsSelected = false;
             MainPage.IsSelected = false;
             CatalogPage.IsSelected = false;
@@ -129,9 +176,29 @@ namespace OnlineShop
         }
 
         private async void StartSearch(object sender, RoutedEventArgs e)
-        {
-            List<Product> products =await ProductProcessor.FromTitle(Search.Text);
-
+        {  
+            if (Search.Text != "")
+            {
+                MainLogic.Visibility = Visibility.Collapsed;
+                MainSearch.Visibility = Visibility.Visible;
+                MainSearch.Children.Clear();
+                List<Product> products = await ProductProcessor.FromTitle(Search.Text);
+                if (products.Count!=0)
+                {
+                    for (int i = 0; i < products.Count; i++)
+                    {
+                        MainSearch.Children.Add(LoadPage.LoadProductButton(products[i]));
+                    }
+                }
+                else
+                {
+                    Label label= new Label();
+                    label.FontSize = 40;
+                    label.Background = Brushes.DarkGray;
+                    label.Content = "Ничего не нашлось.  :(";
+                    MainSearch.Children.Add(label);
+                }
+            }
         }
     }
 }
