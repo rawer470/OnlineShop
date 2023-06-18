33dec1aa0afdf265be12750f34bb6b82c3153966
rawer470 <75203120+rawer470@users.noreply.github.com>
2023-06-18 09:01:43 +0300

    Models for Korzina 1

---
+++ OnlineShop/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class Address
+    {
+        public Geolocation Geolocation { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string Zipcode { get; set; }
+    }
+}
---
+++ OnlineShop/Models/Geolocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class Geolocation
+    {
+        public float Lat { get; set; }
+        public float Long { get; set; }
+    }
+}
